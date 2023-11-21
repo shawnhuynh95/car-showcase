1f1960d3b3f4e1f12c4516fc7583c7b6a3932764
shawnhuynh95 <138313317+shawnhuynh95@users.noreply.github.com>
2023-11-21 22:02:39 +0700

    Update Searchbar.tsx

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import SearchManufacturer from "./SearchManuFacturer";
+import SearchManufacturer from "./SearchManufacturer";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
